2bc4b29bb44ad7b22c01a83f37241ca5ce2fee70
zhanggaobo <zhanggaobo>
2018-08-16 15:32:06 +0800

    123

---
+++ component_base/src/main/java/com/gary/library/component_base/base/mvp/inter/IView.java
@@ -0,0 +1,26 @@
+package com.gary.library.component_base.base.mvp.inter;
+
+import android.arch.lifecycle.LifecycleOwner;
+import android.content.Context;
+
+/**
+ * Created by XQ Yang on 2017/8/8  11:24.
+ * Description : 顶级view接口
+ */
+
+public interface IView extends LifecycleOwner {
+
+    Context getContext();
+
+    void showError(String msg, String code);
+
+    /**
+     * 显示Dialog
+     */
+    void showHUD(String msg);
+
+    /**
+     * 关闭Dialog
+     */
+    void dismissHUD();
+}
